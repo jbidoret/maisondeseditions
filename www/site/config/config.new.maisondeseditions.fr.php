5592ec8c74002b804ba5d67beadc8a98173fa51b
Julien Bidoret <julienbidoret@gmail.com>
2020-01-21 19:43:20 +0100

    Config new.maisondeseditions.fr

---
+++ www/site/config/config.new.maisondeseditions.fr.php
@@ -0,0 +1,14 @@
+<?php
+
+
+return [
+    'debug' => true,
+    'whoops' => true,
+    'cache' => true,
+    'language.detect' => true,
+    'schnti.cachebuster.active' => true,
+    'environment' => 'preprod',
+    'ww.merx.production' => true,
+    'ww.merx.ordersPage' => 'orders',
+    'panel.install' => true,
+];
