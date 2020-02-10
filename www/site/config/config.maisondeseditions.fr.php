1ab457640f05bdbf07bdf16391855ad27935837f
Julien Bidoret <julienbidoret@gmail.com>
2020-02-10 15:06:02 +0100

    Prod config

---
+++ www/site/config/config.maisondeseditions.fr.php
@@ -0,0 +1,13 @@
+<?php
+
+
+return [
+    'debug' => false,
+    'whoops' => false,
+    'cache' => true,
+    'language.detect' => true,
+    'schnti.cachebuster.active' => true,
+    'environment' => 'production',
+    'ww.merx.production' => true,
+    'ww.merx.ordersPage' => 'orders',
+];
