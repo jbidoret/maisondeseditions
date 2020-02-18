d5d88e9efde1e8def16ecd54b2e060cdea8f83f9
Julien Bidoret <julienbidoret@gmail.com>
2020-02-18 22:01:10 +0100

    Cachebuster off

--- www/site/config/config.maisondeseditions.fr.php
+++ www/site/config/config.maisondeseditions.fr.php
@@ -6,7 +6,7 @@ return [
     'whoops' => false,
     'cache' => true,
     'language.detect' => true,
-    'schnti.cachebuster.active' => true,
+    #'schnti.cachebuster.active' => true,
     'environment' => 'production',
     'ww.merx.production' => true,
     'ww.merx.ordersPage' => 'orders',
